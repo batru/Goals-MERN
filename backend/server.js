17aae24d6c2be53b37cfc31001f42854cc20f109
Batrudin <batrudin10@gmail.com>
2024-01-01 19:22:29 +0300

    REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorHandler');
-
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
+
+//connect DB
+connectDB();
 
 const app = express();
 
